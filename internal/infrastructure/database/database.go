600ed71e16276489850f6cd469b1ce25d562fd45
Gaevoy <gaevoyrs@gmail.com>
2023-11-06 21:00:51 +0300

    close #3 fix connection to db

--- internal/infrastructure/database/database.go
+++ internal/infrastructure/database/database.go
@@ -7,15 +7,21 @@ import (
 	"os"
 )
 
+var connect *sqlx.DB
+
 func NewConnect() (*sqlx.DB, error) {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Moscow",
-		os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_DB"),
-		os.Getenv("POSTGRES_PORT_EXTERNAL"))
-	return sqlx.Connect("pgx", dsn)
+	var err error
+	if connect == nil {
+		dsn := fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Moscow",
+			os.Getenv("POSTGRES_HOST"),
+			os.Getenv("POSTGRES_USER"),
+			os.Getenv("POSTGRES_PASSWORD"),
+			os.Getenv("POSTGRES_DB"),
+			os.Getenv("POSTGRES_PORT_EXTERNAL"))
+		connect, err = sqlx.Connect("pgx", dsn)
+	}
+	return connect, err
 }
 
 func NewConnectTest() (*sqlx.DB, error) {
