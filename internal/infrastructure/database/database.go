59d713dd568ce7d279a60bc349390fff61b0a740
Gaevoy <gaevoyrs@gmail.com>
2023-11-06 21:16:01 +0300

    close #3

--- internal/infrastructure/database/database.go
+++ internal/infrastructure/database/database.go
@@ -7,11 +7,11 @@ import (
 	"os"
 )
 
-var connect *sqlx.DB
+var Connect *sqlx.DB
 
 func NewConnect() (*sqlx.DB, error) {
 	var err error
-	if connect == nil {
+	if Connect == nil {
 		dsn := fmt.Sprintf(
 			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Moscow",
 			os.Getenv("POSTGRES_HOST"),
@@ -19,9 +19,9 @@ func NewConnect() (*sqlx.DB, error) {
 			os.Getenv("POSTGRES_PASSWORD"),
 			os.Getenv("POSTGRES_DB"),
 			os.Getenv("POSTGRES_PORT_EXTERNAL"))
-		connect, err = sqlx.Connect("pgx", dsn)
+		Connect, err = sqlx.Connect("pgx", dsn)
 	}
-	return connect, err
+	return Connect, err
 }
 
 func NewConnectTest() (*sqlx.DB, error) {
